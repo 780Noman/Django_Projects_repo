7b1c52d61ee851b9e3b8f89344fca3061f3d5b2e
Noman Amjad <nq6145841@gmail.com>
2024-10-05 18:27:24 +0500

    main

--- feedback/feedback/urls.py
+++ feedback/feedback/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 
-
 urlpatterns = [
     path('', surveys,name='surveys'),
     path('survey_view/<id>/', survey_view,name='survey_view'),
